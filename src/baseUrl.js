54b28d8d6bcbef5a755d339793572ff43f6d7d78
begjooo <begjooo@gmail.com>
2025-09-15 10:13:58 +0700

    update

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,9 +1,9 @@
 // export const baseUrl = 'http://localhost:3000';
-export const baseUrl = 'http://192.168.80.132:3000';
+// export const baseUrl = 'http://192.168.80.132:3000';
 // export const baseUrl = 'http://192.168.1.161:3000';
 
 // export const baseUrl = 'https://localhost:3000';
 // export const baseUrl = 'https://192.168.80.132:3000';
-// export const baseUrl = 'https://192.168.1.161:3000';
+export const baseUrl = 'https://192.168.1.161:3000';
 
 // export const baseUrl = 'https://7b2d8114fb4f.ngrok-free.app'; // ngrok backend url
